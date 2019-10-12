fb7c09819cc81903073d5cfeb294195c693754a4
Разиков Алексей <razikov88@yandex.ru>
2019-10-13 00:28:40 +0300

    update

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -20,95 +20,6 @@ class SiteController extends Controller
 
         return parent::beforeAction($action);
     }
-    public function actions()
-    {
-        return [
-            'error' => [
-                'class' => 'yii\web\ErrorAction',
-            ],
-        ];
-    }
-    
-    public function actionWorkflow()
-    {
-        $registry = new \Symfony\Component\Workflow\Registry();
-        
-        $definitionBuilder = new \Symfony\Component\Workflow\DefinitionBuilder();
-        $definition = $definitionBuilder->addPlaces(['draft', 'review', 'rejected', 'published'])
-            ->addTransition(new \Symfony\Component\Workflow\Transition('to_review', 'draft', 'review'))
-            ->addTransition(new \Symfony\Component\Workflow\Transition('publish', 'review', 'published'))
-            ->addTransition(new \Symfony\Component\Workflow\Transition('reject', 'review', 'rejected'))
-            ->build()
-        ;
-        $marking = new \Symfony\Component\Workflow\MarkingStore\SingleStateMarkingStore('currentState');
-        $workflow = new \Symfony\Component\Workflow\Workflow($definition, $marking, null, 'articleFlow');
-        $registry->addWorkflow($workflow, new \Symfony\Component\Workflow\SupportStrategy\InstanceOfSupportStrategy(\app\models\Articles::class));
-        
-        $definitionBuilder = new \Symfony\Component\Workflow\DefinitionBuilder();
-        $definition = $definitionBuilder->addPlaces(['draft', 'wait_for_journalist', 'approved_by_journalist', 'wait_for_spellchecker', 'approved_by_spellchecker', 'testAlternative', 'published'])
-            ->addTransition(new \Symfony\Component\Workflow\Transition('request_review', 'draft', ['wait_for_journalist', 'wait_for_spellchecker', 'testAlternative']))
-            ->addTransition(new \Symfony\Component\Workflow\Transition('request_review2', 'draft', 'testAlternative'))
-            ->addTransition(new \Symfony\Component\Workflow\Transition('journalist_approval', 'wait_for_journalist', 'approved_by_journalist'))
-            ->addTransition(new \Symfony\Component\Workflow\Transition('spellchecker_approval', 'wait_for_spellchecker', 'approved_by_spellchecker'))
-            ->addTransition(new \Symfony\Component\Workflow\Transition('publish', ['approved_by_journalist', 'approved_by_spellchecker'], 'published'))
-            ->addTransition(new \Symfony\Component\Workflow\Transition('publish2', 'testAlternative', 'published'))
-            ->build()
-        ;
-
-        $marking = new \Symfony\Component\Workflow\MarkingStore\MultipleStateMarkingStore('currentState');
-        $workflow = new \Symfony\Component\Workflow\Workflow($definition, $marking, null, 'articleMultipleFlow');
-        $registry->addWorkflow($workflow, new \Symfony\Component\Workflow\SupportStrategy\InstanceOfSupportStrategy(\app\models\Articles::class));
-        //===================
-        $post = new \app\models\Articles();
-        $workflow = $registry->get($post, 'articleFlow');
-        var_dump($post->currentState);
-        var_dump($workflow->can($post, 'publish'));
-        var_dump($workflow->can($post, 'to_review'));
-        $workflow->apply($post, 'to_review');
-        var_dump($post->currentState);
-        var_dump($workflow->can($post, 'publish'));
-        var_dump($workflow->can($post, 'to_review'));
-        //===================
-        var_dump("===================");
-        $post = new \app\models\Articles();
-        $workflow = $registry->get($post, 'articleMultipleFlow');
-        var_dump($post->currentState);
-        var_dump($workflow->can($post, 'request_review'));
-        var_dump($workflow->can($post, 'journalist_approval'));
-        var_dump($workflow->can($post, 'approved_by_journalist'));
-        var_dump($workflow->can($post, 'spellchecker_approval'));
-        var_dump($workflow->can($post, 'approved_by_spellchecker'));
-        var_dump($workflow->can($post, 'testAlternative'));
-        var_dump($workflow->can($post, 'publish'));
-        var_dump($workflow->can($post, 'publish2'));
-//        $workflow->apply($post, 'request_review');
-        $workflow->apply($post, 'request_review2');
-        var_dump($post->currentState);
-        var_dump($workflow->can($post, 'request_review'));
-        var_dump($workflow->can($post, 'journalist_approval'));
-        var_dump($workflow->can($post, 'approved_by_journalist'));
-        var_dump($workflow->can($post, 'spellchecker_approval'));
-        var_dump($workflow->can($post, 'approved_by_spellchecker'));
-        var_dump($workflow->can($post, 'testAlternative'));
-        var_dump($workflow->can($post, 'publish'));
-        var_dump($workflow->can($post, 'publish2'));
-//        $workflow->apply($post, 'journalist_approval');
-//        var_dump($post->currentState);
-//        var_dump($workflow->can($post, 'request_review'));
-//        var_dump($workflow->can($post, 'journalist_approval'));
-//        var_dump($workflow->can($post, 'approved_by_journalist'));
-//        var_dump($workflow->can($post, 'spellchecker_approval'));
-//        var_dump($workflow->can($post, 'approved_by_spellchecker'));
-//        var_dump($workflow->can($post, 'testAlternative'));
-//        var_dump($workflow->can($post, 'publish'));
-        
-        exit;
-    }
-    
-    public function actionElement()
-    {
-        return $this->render('element', []);
-    }
     
     public function actionIndex()
     {
@@ -117,40 +28,12 @@ class SiteController extends Controller
             $data = NestedSetsHelper::generate($form->number);
             NestedSets::deleteAll();
             Yii::$app->db->createCommand()->batchInsert('nestedsets', ['id', 'name', 'lft', 'rgt', 'lvl'], $data)->execute();
+            $this->redirect('');
         }
 
         $tree = NestedSets::findDefault();
 
         return $this->render('index', ['model' => $form, 'items' => $tree]);
-    }
-    
-    public function actionModal()
-    {
-        $provider = new \yii\data\ActiveDataProvider([
-            'query' => \app\models\Articles::find(),
-            'pagination' => [
-                'pageSize' => 20,
-            ],
-        ]);
-        return $this->render('modal', ['dataProvider' => $provider]);
-    }
-    
-    public function actionUpdateModalArticle($id)
-    {
-        $model = \app\models\Articles::find()->where(['id' => $id])->one();
-        if (!$model) {
-            throw new NotFoundHttpException('Запись не найдена');
-        }
-        Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return ['model' => $model];
-        }
-        return ['form' => $this->renderPartial('//article/updateModal', ['model' => $model])];
-    }
-    
-    public function actionDesk()
-    {
-        return $this->render('desk', []);
     }
     
     public function actionTree()
@@ -166,10 +49,10 @@ class SiteController extends Controller
         var_dump(\yii\helpers\ArrayHelper::getValue($treeData, $getKey), $availablePaths, $treeData['rows'][6]['rows'][2]);
     }
     
-//    public function actionError()
-//    {
-//        return $this->redirect('/');
-//    }
+    public function actionError()
+    {
+        return $this->redirect('/');
+    }
     
     public function actionNestedSets()
     {
