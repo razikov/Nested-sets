ebff741e115eef7e555843075877a11054460a2b
razikov <razikov88@yandex.ru>
2020-04-10 16:46:30 +0300

    Delete db.php

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=tests',
-    'username' => 'root',
-    'password' => 'hexrf88',
-    'charset' => 'utf8',
-];
